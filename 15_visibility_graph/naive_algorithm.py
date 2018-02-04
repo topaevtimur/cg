17efae26c56140a5946817f3aa951b7c9635dcff
topaevtimur <topaev-timur@mail.ru>
2018-02-04 22:07:28 +0300

    some fix

--- 15_visibility_graph/naive_algorithm.py
+++ 15_visibility_graph/naive_algorithm.py
@@ -1,21 +1,17 @@
-from random import randint
 import matplotlib.pyplot as plt
 import numpy as np
-from ipywidgets import interact, interactive, IntSlider
-from IPython.display import display
-from random import random
-from matplotlib.path import Path
-import matplotlib.patches as patches
 from scipy.spatial import ConvexHull
+
+from util.check import check
+from util.create_SP import createSP
+from util.draw_graph import drawGraph
+from util.generate_points import generatePoints
+from util.hull_go import hullGo
+from util.intersect_line import intersect_line
 
 
 def naive_task(solution):
 
-    def generatePoints(x1, x2, y1, y2, N):
-        points = {(randint(x1, x2), randint(y1, y2)) for i in range(N)}
-        while len(points) < N:
-            points |= {(randint(x1, x2), randint(y1, y2))}
-        return list(list(x) for x in points)
 
     first_poly = generatePoints(0, 20, 0, 20, 7)
     second_poly = generatePoints(20, 40, 0, 20, 7)
@@ -24,71 +20,34 @@ def naive_task(solution):
     first_poly_np = np.array(first_poly)
     second_poly_np = np.array(second_poly)
 
-    fig = plt.figure(figsize=(6, 6))
-    ax1 = plt.subplot(111, aspect='equal')
-    ax1.set_xlim(-5,45)
-    ax1.set_ylim(-5,25)
+    fig, ax1 = createSP(plt)
 
-    points = []
-    first_poly_hul = []
-    second_poly_hul = []
+    first_poly_hul, ax1 = hullGo(hull1, ax1, first_poly_np, first_poly)
+    second_poly_hul, ax1 = hullGo(hull2, ax1, second_poly_np, second_poly)
 
-    for simplex in hull1.simplices:
-        ax1.plot(first_poly_np[simplex, 0], first_poly_np[simplex, 1], 'k-')
-    for v in hull1.vertices:
-        first_poly_hul.append(first_poly[v])
-    for simplex in hull2.simplices:
-        ax1.plot(second_poly_np[simplex, 0], second_poly_np[simplex, 1], 'k-')
-    for v in hull2.vertices:
-        second_poly_hul.append(second_poly[v])
+
 
     ans = solution(first_poly_hul, second_poly_hul)
 
-    for v in ans:
-        p1 = v[0]
-        p2 = v[1]
-        ax1.plot([p1[0], p2[0]], [p1[1], p2[1]], color='r', linestyle='-', linewidth=1)
-
-    display(fig)
+    drawGraph(ans, ax1, fig)
 
     ans1 = naive_algorithm(first_poly_hul, second_poly_hul)
 
-    ans.sort()
-    ans1.sort()
-
-    if ans1 == ans:
-        print("Accepted")
-    else:
-        print("WA 1")
+    check(ans, ans1)
 
 def naive_algorithm(first_poly_hul, second_poly_hul):
 
     points = list(first_poly_hul)
     points += second_poly_hul
 
-    def area(a, b, c):
-        return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
-
-    def intersect1(x1, y1, x2, y2):
-        if x1 > y1:
-            x1, y1 = y1, x1
-        if x2 > y2:
-            x2, y2 = y2, x2
-        return max(x1, x2) <= min(y1, y2)
-
-    def intersect_line(a, b, c, d):
-        return (intersect1(a[0], b[0], c[0], d[0])
-            and intersect1(a[1], b[1], c[1], d[1])
-            and area(a, b, c) * area(a, b, d) <= 0
-            and area(c, d, a) * area(c, d, b) <= 0)
 
     ans = [];
 
     for p1 in first_poly_hul:
-        for p2 in second_poly_hul: 
+        for p2 in second_poly_hul:
             t1 = list(p1)
             t2 = list(p2)
-            t1[0] += 0.00001 # TODO 
+            t1[0] += 0.00001 # TODO
             t2[0] -= 0.00001
             intersect = False
 
@@ -108,4 +67,5 @@ def naive_algorithm(first_poly_hul, second_poly_hul):
                 ans.append([t1, t2])
 
     return ans
+
 
--- 15_visibility_graph/sweep_line_algorithm.py
+++ 15_visibility_graph/sweep_line_algorithm.py
@@ -1,21 +1,16 @@
-from random import randint
 import matplotlib.pyplot as plt
 import numpy as np
-from ipywidgets import interact, interactive, IntSlider
-from IPython.display import display
-from random import random
-from matplotlib.path import Path
-import matplotlib.patches as patches
 from scipy.spatial import ConvexHull
+
+from util.check import check
+from util.create_SP import createSP
+from util.draw_graph import drawGraph
+from util.generate_points import generatePoints
+from util.hull_go import hullGo
+from util.intersect_line import intersect_line
 
 
 def sweep_task(solution):
-
-    def generatePoints(x1, x2, y1, y2, N):
-        points = {(randint(x1, x2), randint(y1, y2)) for i in range(N)}
-        while len(points) < N:
-            points |= {(randint(x1, x2), randint(y1, y2))}
-        return list(list(x) for x in points)
 
     polygon = generatePoints(20, 40, 0, 20, 7)
     hull = ConvexHull(polygon)
@@ -23,66 +18,33 @@ def sweep_task(solution):
 
     point = [10, 10]
 
-    fig = plt.figure(figsize=(6, 6))
-    ax1 = plt.subplot(111, aspect='equal')
-    ax1.set_xlim(-5,45)
-    ax1.set_ylim(-5,25)
+    fig, ax1 = createSP(plt);
+
 
     ax1.plot(point[0], point[1], 'ro');
 
-    points = []
-    polygon_hul = []
 
-    for simplex in hull.simplices:
-        ax1.plot(polygon_np[simplex, 0], polygon_np[simplex, 1], 'k-')
-    for v in hull.vertices:
-        polygon_hul.append(polygon[v])
 
-    ans = solution(point, polygon)
+    polygon_hul, ax1 = hullGo(hull, ax1, polygon_np, polygon)
+
+    ans = solution(point, polygon_hul)
 
     print(ans)
 
-    for v in ans:
-        p1 = v[0]
-        p2 = v[1]
-        ax1.plot([p1[0], p2[0]], [p1[1], p2[1]], color='r', linestyle='-', linewidth=1)
+    drawGraph(ans, ax1, fig)
 
-    display(fig)
-
-    ans1 = naive_algorithm(point, polygon)
-
-    ans.sort()
-    ans1.sort()
-
-    if ans1 == ans:
-        print("Accepted")
-    else:
-        print("WA 1")
+    ans1 = naive_algorithm(point, polygon_hul)
+    check(ans, ans1)
 
 def naive_algorithm(point, polygon):
 
     points = list(polygon)
     points += point
 
-    def area(a, b, c):
-        return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
-
-    def intersect1(x1, y1, x2, y2):
-        if x1 > y1:
-            x1, y1 = y1, x1
-        if x2 > y2:
-            x2, y2 = y2, x2
-        return max(x1, x2) <= min(y1, y2)
-
-    def intersect_line(a, b, c, d):
-        return (intersect1(a[0], b[0], c[0], d[0])
-            and intersect1(a[1], b[1], c[1], d[1])
-            and area(a, b, c) * area(a, b, d) <= 0
-            and area(c, d, a) * area(c, d, b) <= 0)
 
     ans = []
 
-    for p2 in polygon: 
+    for p2 in polygon:
         t1 = list(point)
         t2 = list(p2)
         t2[0] -= 0.00001
